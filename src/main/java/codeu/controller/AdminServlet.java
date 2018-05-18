30210288b2de098ed479bc09c3d4933820eecdbe
Daniel <d.mccrystal512@gmail.com>
2018-05-17 21:04:15 -0400

    populated initial files with base structure code

---
+++ src/main/java/codeu/controller/AdminServlet.java
@@ -0,0 +1,29 @@
+package codeu.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AdminServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+
+    @Override 
+    public void doGet(HttpServletRequest request, HttpServletRequest response)
+        throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+
+    }
+}
