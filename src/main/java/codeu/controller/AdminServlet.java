dc4a2053d85c178f0d3c65676cb3a3356a474971
Daniel <d.mccrystal512@gmail.com>
2018-05-26 19:36:21 -0400

    removed test data generation for different PR

--- src/main/java/codeu/controller/AdminServlet.java
+++ src/main/java/codeu/controller/AdminServlet.java
@@ -39,7 +39,6 @@ public class AdminServlet extends HttpServlet {
     setConversationStore(ConversationStore.getInstance());
     setMessageStore(MessageStore.getInstance());
 
-    // addTestData();
   }
 
   void setAdminUsernames() {
@@ -147,51 +146,6 @@ public class AdminServlet extends HttpServlet {
 
   }
 
-  void addTestData() {
-    int numberOfUsers = ThreadLocalRandom.current().nextInt(10, 101);
-    int numberOfConversations = ThreadLocalRandom.current().nextInt(5, 21);
-    int numberOfMessages = ThreadLocalRandom.current().nextInt(100, 1001);
-
-    User newUser;
-    for (int i = 0; i < numberOfUsers; i++) {
-      newUser = new User(UUID.randomUUID(), "user"+i, "password",
-        Instant.ofEpochSecond(ThreadLocalRandom.current().nextInt(1, 10001)));
-
-      userStore.addUser(newUser);
-    }
-
-    Conversation newConversation;
-    User owner;
-    for (int i = 0; i < numberOfConversations; i++) {
-      owner = userStore.getAllUsers().get(ThreadLocalRandom.current().nextInt(0, userStore.getAllUsers().size()));
-      newConversation = new Conversation(UUID.randomUUID(), owner.getId(), "conversation"+i,
-        Instant.now());
-
-      conversationStore.addConversation(newConversation);
-    }
-
-    Message newMessage;
-    Conversation conversation;
-    User author;
-    int numberOfWords;
-    String content;
-    for (int i = 0; i < numberOfMessages; i++) {
-      author = userStore.getAllUsers().get(ThreadLocalRandom.current().nextInt(0, userStore.getAllUsers().size()));
-      conversation = conversationStore.getAllConversations().get(ThreadLocalRandom.current().
-        nextInt(0, conversationStore.getAllConversations().size()));
-
-      numberOfWords = ThreadLocalRandom.current().nextInt(0, 21);
-      content = "" + (char) ThreadLocalRandom.current().nextInt('a', 'z'+1);
-      for (int w = 1; w < numberOfWords; w++) {
-        content += " " + (char) ThreadLocalRandom.current().nextInt('a', 'z'+1);
-      }
-
-      newMessage = new Message(UUID.randomUUID(), conversation.getId(), author.getId(),
-       content, Instant.now());
-
-      messageStore.addMessage(newMessage);
-    }
-  }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
