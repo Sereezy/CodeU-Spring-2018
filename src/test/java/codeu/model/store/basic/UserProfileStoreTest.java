f0e02b78ffa85a862223a4daa801258ac832ed7b
Serena Burton <serenaburton@yahoo.com>
2018-06-16 10:17:56 -0400

    .

--- src/test/java/codeu/model/store/basic/UserProfileStoreTest.java
+++ src/test/java/codeu/model/store/basic/UserProfileStoreTest.java
@@ -2,7 +2,7 @@ package codeu.model.store.basic;
 
 import codeu.model.data.UserProfile;
 import codeu.model.store.persistence.PersistentStorageAgent;
-
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
