76f9e965f1bf5c5939cbdfe26fe776ab9b0f77c4
Serena Burton <serenaburton@yahoo.com>
2018-07-09 17:55:20 -0400

    Uncommenting

--- src/test/java/codeu/controller/ProfileServletTest.java
+++ src/test/java/codeu/controller/ProfileServletTest.java
@@ -1,52 +1,52 @@
-//package codeu.controller;
+package codeu.controller;
 
-//import static org.junit.Assert.fail;
+import static org.junit.Assert.fail;
 
-//import java.io.IOException;
+import java.io.IOException;
 
-//import javax.servlet.RequestDispatcher;
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.mockito.Mockito;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
 
-//public class ProfileServletTest {
+public class ProfileServletTest {
 
-  //  private ProfileServlet profileServlet;
-  //  private HttpServletRequest mockRequest;
-  //  private HttpSession mockSession;
-  //  private HttpServletResponse mockResponse;
-  //  private RequestDispatcher mockRequestDispatcher;
+    private ProfileServlet profileServlet;
+    private HttpServletRequest mockRequest;
+    private HttpSession mockSession;
+    private HttpServletResponse mockResponse;
+    private RequestDispatcher mockRequestDispatcher;
 
-  //  @Before
-  //  public void setup() {
-    //	profileServlet = new ProfileServlet();
-
-
-      //  mockRequest = Mockito.mock(HttpServletRequest.class);
-      //  mockSession = Mockito.mock(HttpSession.class);
-//        Mockito.when(mockRequest.getSession()).thenReturn(mockSession);
+    @Before
+    public void setup() {
+    	profileServlet = new ProfileServlet();
 
 
-      //  mockResponse = Mockito.mock(HttpServletResponse.class);
-        //mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
-        //Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/profile.jsp"))
-      //      .thenReturn(mockRequestDispatcher);
-
-  //  }
+        mockRequest = Mockito.mock(HttpServletRequest.class);
+        mockSession = Mockito.mock(HttpSession.class);
+        Mockito.when(mockRequest.getSession()).thenReturn(mockSession);
 
 
-  //  @Test
-  //  public void testDoGet() throws IOException, ServletException {
-    //  profileServlet.doGet(mockRequest, mockResponse);
+        mockResponse = Mockito.mock(HttpServletResponse.class);
+        mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
+        Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/profile.jsp"))
+            .thenReturn(mockRequestDispatcher);
 
-      //Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
-    //}
+    }
+
+
+    @Test
+    public void testDoGet() throws IOException, ServletException {
+      profileServlet.doGet(mockRequest, mockResponse);
+
+      Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
+    }
 
 
 
-//}
+}
